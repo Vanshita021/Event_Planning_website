ff335c5527a6926371af1729be31ee62fd53c577
Vanshita <93867334+Vanshita021@users.noreply.github.com>
2023-05-08 11:24:17 +0530

    Sample Commit

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+    
+    <!-- copyright -->
+    <div class="agileits-w3layouts">
+        <div class="container">
+            <p>© 2023 VPS Pvt.Ltd. All rights reserved | </p>
+        </div>
+    </div>
+    <!-- //copyright -->
